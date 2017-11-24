fab811d1135d56150c229f187bbae1d8404ad31e
MarySalemme <marysalemme1@gmail.com>
2017-11-24 15:07:52 +0000

    Create App component

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+   return <div>My App</div>;
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
