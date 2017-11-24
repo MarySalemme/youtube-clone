0ebedef36337357cad5dbde3c73ea26db934f9b3
MarySalemme <marysalemme1@gmail.com>
2017-11-24 16:32:36 +0000

    Refactor search bar - set state value

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-   return <input />;
+class SearchBar extends Component {
+   constructor(props) {
+      super(props);
+
+      // only inside the constructor function you can assign state like below
+      this.state = { term: '' };
+   }
+
+   render() {
+      // setState used to change the original state assigned in the constructor
+      return (
+         <div>
+            <input onChange={event => this.setState({ term: event.target.value })} /> <br/>
+            Value of the input: { this.state.term }
+         </div>
+      );
+   }
 }
 
 export default SearchBar;
