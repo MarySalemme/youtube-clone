1573e40630a478cb34fdced57f8795ff33e1e096
MarySalemme <marysalemme1@gmail.com>
2017-11-27 15:37:16 +0000

    Refactor functional compoenent to class compoenent

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCV3Ycw9ZeszeWFsgMj5UtWowwnyA4_6PA';
 
 
-const App = () => {
-   return (
-      <div>
-         <SearchBar />
-      </div>
-   );
+class App extends Component {
+   constructor(props) {
+      super(props);
+      
+      this.state = { videos: [] };
+
+      YTSearch({key: API_KEY, term: 'surfboards'}, function(videos) {
+         this.setState({ videos }); // same as this.setState({ videos: videos})
+      });
+   }
+
+   render() {
+      return (
+         <div>
+            <SearchBar />
+         </div>
+      );
+   }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
