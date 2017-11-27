999bd3c57b809edf43f1879a9cd42aaddfe82d39
MarySalemme <marysalemme1@gmail.com>
2017-11-27 13:54:43 +0000

    Fix typo

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
-const API_KEY = 'AIzaSyCV3Ycw9ZeszeWFsgMj5UtWowwnyA4_6PA'
+
+const API_KEY = 'AIzaSyCV3Ycw9ZeszeWFsgMj5UtWowwnyA4_6PA';
 
 
 const App = () => {
