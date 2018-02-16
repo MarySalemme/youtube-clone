fcaa99dd448765db7759387ff0372db948116939
MarySalemme <marysalemme1@gmail.com>
2018-02-16 13:23:13 +0000

    Add searching videos feature

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,23 +1,28 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-   constructor(props) {
-      super(props);
+  constructor(props) {
+  super(props);
 
-      // only inside the constructor function you can assign state like below
-      this.state = { term: '' };
-   }
-
-   render() {
-      // setState used to change the original state assigned in the constructor
-      return (
-         <div className="search-bar">
-            <input 
-               value={this.state.term}
-               onChange={event => this.setState({ term: event.target.value })} />
-         </div>
-      );
-   }
+  // only inside the constructor function you can assign state like below
+  this.state = { term: '' };
 }
 
-export default SearchBar;
+  render() {
+    // setState used to change the original state assigned in the constructor
+    return (
+      <div className="search-bar">
+        <input
+          value={this.state.term}
+          onChange={event => this.onInputChange(event.target.value)} />
+      </div>
+    );
+  }
+
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+  }
+}
+
+export default SearchBar; 
